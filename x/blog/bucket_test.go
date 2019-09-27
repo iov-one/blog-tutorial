02eaad02baa34ecb9a4f796554017392f9bb241b
Orkun Külçe <kulceorkun@gmail.com>
2019-09-27 10:32:21 +0300

    x/blog: add blogTimedIndexer to article bucket

--- x/blog/bucket_test.go
+++ x/blog/bucket_test.go
@@ -109,6 +109,84 @@ func TestArticleBlogIDIndexer(t *testing.T) {
 	}
 }
 
+func TestBlogTimedIndexer(t *testing.T) {
+	now := weave.AsUnixTime(time.Unix(1, 0))
+	invalidTime := weave.AsUnixTime(time.Unix(-1, 0))
+	future := now.Add(time.Hour)
+
+	blogID := weavetest.SequenceID(1)
+
+	article := &Article{
+		Metadata:     &weave.Metadata{Schema: 1},
+		ID:           weavetest.SequenceID(1),
+		BlogID:       blogID,
+		Title:        "Best hacker's blog",
+		Content:      "Best description ever",
+		CommentCount: 1,
+		LikeCount:    2,
+		CreatedAt:    now,
+		DeleteAt:     future,
+	}
+
+	invalidArticle := &Article{
+		Metadata:     &weave.Metadata{Schema: 1},
+		ID:           weavetest.SequenceID(1),
+		BlogID:       blogID,
+		Title:        "Best hacker's blog",
+		Content:      "Best description ever",
+		CommentCount: 1,
+		LikeCount:    2,
+		CreatedAt:    invalidTime,
+		DeleteAt:     future,
+	}
+
+	// the index is by article and time, not by the blog
+	successCaseExpectedValue := []byte{0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1}
+
+	cases := map[string]struct {
+		obj      orm.Object
+		expected []byte
+		wantErr  *errors.Error
+	}{
+		"success": {
+			obj:      orm.NewSimpleObj(nil, article),
+			expected: successCaseExpectedValue,
+			wantErr:  nil,
+		},
+		"failure obj is nil": {
+			obj:      nil,
+			expected: nil,
+			wantErr:  nil,
+		},
+		"not article": {
+			obj:      orm.NewSimpleObj(nil, new(Blog)),
+			expected: nil,
+			wantErr:  errors.ErrState,
+		},
+		"empty obj has nil value": {
+			obj:      orm.NewSimpleObj(nil, nil),
+			expected: nil,
+			wantErr:  nil,
+		},
+		"invalid creation time": {
+			obj:      orm.NewSimpleObj(nil, invalidArticle),
+			expected: nil,
+			wantErr:  errors.ErrState,
+		},
+	}
+
+	for testName, tc := range cases {
+		t.Run(testName, func(t *testing.T) {
+			index, err := blogTimedIndexer(tc.obj)
+
+			if !tc.wantErr.Is(err) {
+				t.Fatalf("unexpected error: %+v", err)
+			}
+			assert.Equal(t, tc.expected, index)
+		})
+	}
+}
+
 func TestCommentArticleIDIndexer(t *testing.T) {
 	now := weave.AsUnixTime(time.Now())
 
